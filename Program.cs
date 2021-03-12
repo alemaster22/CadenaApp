16d6deb9c49103566bd154b0de763ce59a3a9a2b
alemaster22 <alemaster222@gmail.com>
2021-03-11 23:31:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CadenaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un texto para procesar: ");
+
+            string cadena = Console.ReadLine();
+
+            string[] splitString = Regex.Split(cadena, @"");
+
+            StringBuilder myStringBuilder = new StringBuilder("");       
+
+            for (int i= (splitString.Length-1); i >0; i--)
+            {
+                myStringBuilder.Append(splitString[i]);
+            }
+            Console.WriteLine("El texto procesado es :  " + myStringBuilder);
+
+            Console.ReadLine();
+        }
+    }
+}
